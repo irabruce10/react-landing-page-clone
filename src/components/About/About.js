630c83f36c13c53521afee6e851ebdf1a1bb5b89
Bruce <irabruce10@gmail.com>
2024-09-25 15:22:17 +0200

    Styling Header

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,65 +1,62 @@
 import Button from '../Button/Button';
-
+import styles from './About.module.scss';
 function About() {
   return (
-    <div>
-      <div>
-        <div>
-          <img
-            src="https://res.cloudinary.com/stitch-fix/image/upload/q_auto/v1718416285/landing-pages/pages/US/gateway/SITE-FY24-Gateway%20Update%20V1%20and%20V1.5%20-1207050307893278/2_HIW/HIW-1-Top.svg"
-            width="224"
-            height="85"
-          />
-          <div>1.</div>
-          <h3>Take your style quiz</h3>
-          <p>
-            Tell us about your style, sizes and budget—the more we know, the
-            more your men’s Stylist can help.
-          </p>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1>It's easy</h1>
+        <h2>Here's how it works</h2>
+        <div className={styles.items}>
+          <div>
+            <img src="https://res.cloudinary.com/stitch-fix/image/upload/q_auto/v1718416285/landing-pages/pages/US/gateway/SITE-FY24-Gateway%20Update%20V1%20and%20V1.5%20-1207050307893278/2_HIW/HIW-1-Top.svg" />
 
-          <img src="https://res.cloudinary.com/stitch-fix/image/upload/q_auto/v1719601200/landing-pages/pages/US/gateway/SITE-FY24-Gateway%20Update%20V1%20and%20V1.5%20-1207050307893278/2_HIW/HIW-1-Bottom.svg" />
-        </div>
+            <h3>Take your style quiz</h3>
+            <p>
+              Tell us about your style, sizes and budget—the more we know, the
+              more your men’s Stylist can help.
+            </p>
 
-        <div>
-          <img
-            src="https://res.cloudinary.com/stitch-fix/image/upload/q_auto/v1718416284/landing-pages/pages/US/gateway/SITE-FY24-Gateway%20Update%20V1%20and%20V1.5%20-1207050307893278/2_HIW/HIW-2-Top.svg"
-            width="224"
-            height="85"
-          />
-          <div>1.</div>
-          <h3>2. Match with your (human!) Stylist</h3>
-          <p>
-            Let your Stylist know what you’re looking for. They’ll select 5
-            personalized pieces, and send your men's clothing box straight to
-            your door.
-          </p>
+            <img src="https://res.cloudinary.com/stitch-fix/image/upload/q_auto/v1719601200/landing-pages/pages/US/gateway/SITE-FY24-Gateway%20Update%20V1%20and%20V1.5%20-1207050307893278/2_HIW/HIW-1-Bottom.svg" />
+          </div>
+          <div>
+            <img
+              src="https://res.cloudinary.com/stitch-fix/image/upload/q_auto/v1718416284/landing-pages/pages/US/gateway/SITE-FY24-Gateway%20Update%20V1%20and%20V1.5%20-1207050307893278/2_HIW/HIW-2-Top.svg"
+              width="224"
+              height="85"
+            />
+            <div>1.</div>
+            <h3>2. Match with your (human!) Stylist</h3>
+            <p>
+              Let your Stylist know what you’re looking for. They’ll select 5
+              personalized pieces, and send your men's clothing box straight to
+              your door.
+            </p>{' '}
+            <img
+              src="https://res.cloudinary.com/stitch-fix/image/upload/q_auto/v1718416283/landing-pages/pages/US/gateway/SITE-FY24-Gateway%20Update%20V1%20and%20V1.5%20-1207050307893278/2_HIW/HIW-2-Bottom.svg"
+              width="224"
+              height="22"
+            />
+          </div>
+          <div>
+            <img
+              src="https://res.cloudinary.com/stitch-fix/image/upload/q_auto/v1719601201/landing-pages/pages/US/gateway/SITE-FY24-Gateway%20Update%20V1%20and%20V1.5%20-1207050307893278/2_HIW/HIW-3-Top.svg"
+              width="224"
+              height="85"
+            />
+            <div>1.</div>
+            <h3>3. Get your personalized Fix</h3>
+            <p>
+              There’s a $20 styling fee per box (aka "Fix") order, which is then
+              credited to whatever you choose to keep. Shipping and returns are
+              always free.
+            </p>
 
-          <img
-            src="https://res.cloudinary.com/stitch-fix/image/upload/q_auto/v1718416283/landing-pages/pages/US/gateway/SITE-FY24-Gateway%20Update%20V1%20and%20V1.5%20-1207050307893278/2_HIW/HIW-2-Bottom.svg"
-            width="224"
-            height="22"
-          />
-        </div>
-
-        <div>
-          <img
-            src="https://res.cloudinary.com/stitch-fix/image/upload/q_auto/v1719601201/landing-pages/pages/US/gateway/SITE-FY24-Gateway%20Update%20V1%20and%20V1.5%20-1207050307893278/2_HIW/HIW-3-Top.svg"
-            width="224"
-            height="85"
-          />
-          <div>1.</div>
-          <h3>3. Get your personalized Fix</h3>
-          <p>
-            There’s a $20 styling fee per box (aka "Fix") order, which is then
-            credited to whatever you choose to keep. Shipping and returns are
-            always free.
-          </p>
-
-          <img
-            src="https://res.cloudinary.com/stitch-fix/image/upload/q_auto/v1718416287/landing-pages/pages/US/gateway/SITE-FY24-Gateway%20Update%20V1%20and%20V1.5%20-1207050307893278/2_HIW/HIW-3-Bottom.svg"
-            width="224"
-            height="25"
-          />
+            <img
+              src="https://res.cloudinary.com/stitch-fix/image/upload/q_auto/v1718416287/landing-pages/pages/US/gateway/SITE-FY24-Gateway%20Update%20V1%20and%20V1.5%20-1207050307893278/2_HIW/HIW-3-Bottom.svg"
+              width="224"
+              height="25"
+            />
+          </div>
         </div>
       </div>
 
