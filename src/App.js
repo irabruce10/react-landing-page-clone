3841fb8e935932f18abd5461d3cb75dbd57003eb
Bruce <irabruce10@gmail.com>
2024-09-25 15:05:15 +0200

    Styling Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import About from './components/About/About';
 import Header from './components/Header/Header';
+import Products from './components/Product/Products';
 
 export default function App() {
   return (
     <>
       <Header />
+      <About />
+      <Products />
     </>
   );
 }
---
+++ src/components/Product/Products.js
@@ -0,0 +1,58 @@
+function Products() {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>What you get</h1>
+          <h2>Your style on your budget</h2>
+
+          <div>
+            <div>
+              <img
+                src="https://res.cloudinary.com/stitch-fix/image/upload/f_auto,q_auto/v1724105606/landing-pages/pages/US/men/Rebrand%20Launch%20'24/3_M_WIAF_1.jpg"
+                width="500"
+                height="503"
+              />
+            </div>
+            <div>
+              <img
+                src="https://res.cloudinary.com/stitch-fix/image/upload/f_auto,q_auto/v1719602781/landing-pages/pages/US/men/All24-WEB-LOB%20Gateway%20Update%20-1207339213227223-Men/3_WIAF/3_M_WIAF_3.png"
+                width="500"
+                height="500"
+              />
+            </div>
+            <img
+              src="https://res.cloudinary.com/stitch-fix/image/upload/f_auto,q_auto/v1719602781/landing-pages/pages/US/men/All24-WEB-LOB%20Gateway%20Update%20-1207339213227223-Men/3_WIAF/3_M_WIAF_1.png"
+              width="500"
+              height="500"></img>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <img
+            src="https://res.cloudinary.com/stitch-fix/image/upload/f_auto,q_auto/v1724105603/landing-pages/pages/US/men/Rebrand%20Launch%20'24/4_M_Price_1.jpg"
+            width="400"
+            height="400"
+          />
+        </div>
+        <div>
+          <img
+            a
+            src="https://res.cloudinary.com/stitch-fix/image/upload/f_auto,q_auto/v1724105602/landing-pages/pages/US/men/Rebrand%20Launch%20'24/4_M_Price_2.jpg"
+            width="400"
+            height="400"
+          />
+        </div>
+        <img
+          src="https://res.cloudinary.com/stitch-fix/image/upload/f_auto,q_auto/v1724105598/landing-pages/pages/US/men/Rebrand%20Launch%20'24/4_M_Price_3.jpg"
+          width="400"
+          height="400"
+        />
+      </div>
+      Products
+    </div>
+  );
+}
+
+export default Products;
