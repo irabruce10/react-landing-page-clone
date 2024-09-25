e568ed3a53ba3b69ea7c3704764a7fac601b8165
Bruce <irabruce10@gmail.com>
2024-09-25 15:51:20 +0200

    Styling Header

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,13 +7,17 @@ function About() {
         <h1>It's easy</h1>
         <h2>Here's how it works</h2>
         <div className={styles.items}>
-          <div>
+          <div className={styles.box}>
             <img src="https://res.cloudinary.com/stitch-fix/image/upload/q_auto/v1718416285/landing-pages/pages/US/gateway/SITE-FY24-Gateway%20Update%20V1%20and%20V1.5%20-1207050307893278/2_HIW/HIW-1-Top.svg" />
 
-            <h3>Take your style quiz</h3>
+            <h3>
+              1. Take your <br /> style quiz
+            </h3>
             <p>
-              Tell us about your style, sizes and budget—the more we know, the
-              more your men’s Stylist can help.
+              Tell us about your style, sizes and <br />
+              budget—the more we know, the more
+              <br />
+              your men’s Stylist can help.
             </p>
 
             <img src="https://res.cloudinary.com/stitch-fix/image/upload/q_auto/v1719601200/landing-pages/pages/US/gateway/SITE-FY24-Gateway%20Update%20V1%20and%20V1.5%20-1207050307893278/2_HIW/HIW-1-Bottom.svg" />
@@ -24,11 +28,14 @@ function About() {
               width="224"
               height="85"
             />
-            <div>1.</div>
-            <h3>2. Match with your (human!) Stylist</h3>
+            <h3>
+              2. Match with your
+              <br /> (human!) Stylist
+            </h3>
             <p>
-              Let your Stylist know what you’re looking for. They’ll select 5
-              personalized pieces, and send your men's clothing box straight to
+              Let your Stylist know what you’re <br /> looking for. They’ll
+              select 5 personalized
+              <br /> pieces, and send your men's clothing <br /> box straight to
               your door.
             </p>{' '}
             <img
@@ -43,12 +50,15 @@ function About() {
               width="224"
               height="85"
             />
-            <div>1.</div>
-            <h3>3. Get your personalized Fix</h3>
+
+            <h3>
+              3. Get your <br /> personalized Fix
+            </h3>
             <p>
-              There’s a $20 styling fee per box (aka "Fix") order, which is then
-              credited to whatever you choose to keep. Shipping and returns are
-              always free.
+              There’s a $20 styling fee per box (aka <br />
+              "Fix") order, which is then credited to
+              <br /> whatever you choose to keep. Shipping <br /> and returns
+              are always free.
             </p>
 
             <img
