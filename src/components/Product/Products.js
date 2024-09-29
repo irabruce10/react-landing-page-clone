9f60e6eaec598635b19c4880022496f396b54c85
Bruce <irabruce10@gmail.com>
2024-09-29 11:16:29 +0200

    Adding About section

--- src/components/Product/Products.js
+++ src/components/Product/Products.js
@@ -11,21 +11,21 @@ function Products() {
             <div className={styles.card_item}>
               <img
                 src="https://res.cloudinary.com/stitch-fix/image/upload/f_auto,q_auto/v1724105606/landing-pages/pages/US/men/Rebrand%20Launch%20'24/3_M_WIAF_1.jpg"
-                width="500"
-                height="503"
+                width="350"
+                height="480"
               />
             </div>
             <div>
               <img
                 src="https://res.cloudinary.com/stitch-fix/image/upload/f_auto,q_auto/v1719602781/landing-pages/pages/US/men/All24-WEB-LOB%20Gateway%20Update%20-1207339213227223-Men/3_WIAF/3_M_WIAF_3.png"
-                width="500"
-                height="500"
+                width="350"
+                height="480"
               />
             </div>
             <img
               src="https://res.cloudinary.com/stitch-fix/image/upload/f_auto,q_auto/v1719602781/landing-pages/pages/US/men/All24-WEB-LOB%20Gateway%20Update%20-1207339213227223-Men/3_WIAF/3_M_WIAF_1.png"
-              width="500"
-              height="500"></img>
+              width="350"
+              height="480"></img>
           </div>
         </div>
       </div>
